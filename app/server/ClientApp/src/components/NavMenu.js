172f62e86a537dd167b87cc27e9b1d4f4ebd869e
hamed Mohammedi <hammedmohamadi@gmail.com>
2018-09-12 00:11:47 +0430

    NaveMenu Style

--- app/server/ClientApp/src/components/NavMenu.js
+++ app/server/ClientApp/src/components/NavMenu.js
@@ -2,6 +2,7 @@
 import { Link } from 'react-router-dom';
 import { Glyphicon, Nav, Navbar, NavItem } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
+import ReactTooltip from 'react-tooltip';
 import history from './history'
 import './NavMenu.css';
 export class NavMenu extends Component {
@@ -11,17 +12,62 @@ export class NavMenu extends Component {
     this.state = { Collapse: false, IsLogin: true };
   }
   IsLogin() {
-    return true;
+    return this.state.IsLogin;
   }
   render() {
-    let LoginView = this.state.IsLogin ?
+    let LoginView = this.IsLogin() ?
       <button onClick={() => this.setState({ IsLogin: !this.state.IsLogin })} className="btn btn-success NewButtons">
         <Glyphicon glyph='user' style={{ marginRight: '5px', marginTop: '3px' }} />Sign In
       </button> :
-      <button onClick={() => this.setState({ IsLogin: !this.state.IsLogin })} className="btn btn-warning NewButtons">
-        <Glyphicon glyph='off' style={{ marginRight: '5px', marginTop: '3px' }} />Sign Out Hamed
-      </button>
+      <div>
+        <div onClick={() => this.setState({ IsLogin: !this.state.IsLogin })} className="ToolTipe">
+          <Glyphicon glyph='off' style={{ marginRight: '5px', marginTop: '3px' }} 
+          data-tip="Sign Out"
+          data-place="bottom"
+          data-type="light"
+          />
+          <span className='ToolTipeText'>Sign Out</span>
+        </div>
+        <div onClick={() => this.setState({ IsLogin: !this.state.IsLogin })} className="ToolTipe">
+          <Glyphicon glyph='cog' style={{ marginRight: '5px', marginTop: '3px' }} 
+          data-tip="Account Settings"
+          data-place="bottom"
+          data-type="light"
+          />
+           <span className='ToolTipeText'>Account</span>
+          <ReactTooltip />
+        </div>
+        <div onClick={() => this.setState({ IsLogin: !this.state.IsLogin })} className="ToolTipe">
+          <Glyphicon glyph='bell' style={{ marginRight: '5px', marginTop: '3px' }} 
+          data-tip="Notifications"
+          data-place="bottom"
+          data-type="light"
+          />
+          <span className='ToolTipeText'>Notifs</span>
+        </div>
+      </div>
       ;
+    let DashBord1 = !this.IsLogin() ?
+        <LinkContainer to={'/Inventory'} exact onClick={() => history.push('/Inventory')}>
+          <NavItem>
+            <Glyphicon glyph='inbox' /> Inventory
+        </NavItem>
+        </LinkContainer> :
+      <div></div>;
+      let DashBord2 = !this.IsLogin() ?
+      <LinkContainer to={'/Submit'} exact onClick={() => history.push('/Submit')}>
+        <NavItem>
+          <Glyphicon glyph='plus' /> Submit
+      </NavItem>
+      </LinkContainer> :
+    <div></div>;
+    let DashBord3 = !this.IsLogin() ?
+    <LinkContainer to={'/Transactions'} exact onClick={() => history.push('/Transactions')}>
+      <NavItem>
+        <Glyphicon glyph='transfer' /> Transactions
+    </NavItem>
+    </LinkContainer> :
+  <div></div>;
     return (
       <Navbar inverse fixedTop fluid collapseOnSelect>
         <div className="Logo">
@@ -37,7 +83,16 @@ export class NavMenu extends Component {
                 <Glyphicon glyph='home' /> Home
               </NavItem>
             </LinkContainer>
+            <LinkContainer to={'/Search'} exact onClick={() => history.push('/Search')}>
+              <NavItem>
+                <Glyphicon glyph='search' /> Search
+              </NavItem>
+            </LinkContainer>
+            {DashBord1}
+            {DashBord2}
+            {DashBord3}
           </Nav>
+          <hr className='ThatHr'/>
           <div>
             {LoginView}
           </div>
