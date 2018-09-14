e23e2c771c2d42153fce85e344a3033cf82210f8
hamed Mohammedi <hammedmohamadi@gmail.com>
2018-09-14 21:27:09 +0430

    Img Upload server

--- app/server/ClientApp/src/components/Inventory/Inventory.js
+++ app/server/ClientApp/src/components/Inventory/Inventory.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Glyphicon, Col, Grid, Row } from 'react-bootstrap';
 import { Statics } from '../Statics';
+import { Web3s } from '../Web3/Web3';
 import './Inventory.css';
 export class Inventory extends Component {
     displayName = Inventory.name
--- app/server/Controllers/AssetsController.cs
+++ app/server/Controllers/AssetsController.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -14,12 +18,26 @@ namespace server.Controllers
     public class AssetsController : ControllerBase
     {
         private readonly DBContext _context;
+        private readonly IHostingEnvironment _hostingEnvironment;
 
-        public AssetsController(DBContext context)
+        public AssetsController(DBContext context, IHostingEnvironment hostingEnvironment)
         {
             _context = context;
+            _hostingEnvironment = hostingEnvironment;
         }
-
+        [HttpGet("GetAssets/{id}")]
+        public async Task<IActionResult> GetAssets([FromRoute] Guid id)
+        {
+            var Owner = await _context.Accounts
+            .Include(x => x.Assets)
+            .FirstOrDefaultAsync(x => x.ID == id);
+            if (Owner == null)
+            {
+                return NotFound();
+            }
+            if ((Owner.Assets == null) || (Owner.Assets.Count <= 1)) return NoContent();
+            return Ok(Owner.Assets);
+        }
         // GET: api/Assets/5
         [HttpGet("{id}")]
         public async Task<IActionResult> GetAsset([FromRoute] Guid id)
@@ -108,6 +126,49 @@ namespace server.Controllers
             await _context.SaveChangesAsync();
 
             return Ok(asset);
+        }
+
+        [HttpPost("Upload/{id}"), DisableRequestSizeLimit]
+        public ActionResult UploadFile([FromRoute]Guid id)
+        {
+            try
+            {
+                StringBuilder sb = new StringBuilder("{");
+                short index = 0;
+                foreach (var file in Request.Form.Files)
+                {
+                    string folderName = "Img";
+                    string webRootPath = _hostingEnvironment.ContentRootPath;
+                    string newPath = Path.Combine(webRootPath, folderName);
+                    if (!Directory.Exists(newPath))
+                    {
+                        Directory.CreateDirectory(newPath);
+                    }
+                    string fileName = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName.Trim('"');
+                    var fileFromat = Path.GetExtension(fileName);
+                    string[] formats = new string[] { ".png", ".jpg", ".jpeg" };
+                    if (!formats.Contains(fileFromat.ToLower())) continue;
+                    if (file.Length > 0 && file.Length < 2e6)
+                    {
+                        var Name = Guid.NewGuid().ToString();
+                        sb.Append($"\"{index}\":\"{Name}\",");
+                        string fullPath = Path.Combine(newPath, $"{Name}{fileFromat}");
+                        using (var stream = new FileStream(fullPath, FileMode.Create))
+                        {
+                            file.CopyTo(stream);
+                        }
+                    }
+                    index++;
+                    if (index == 8) break;
+                }
+                sb.Remove(sb.Length - 1, 1);
+                sb.Append("}");
+                return Ok(sb.ToString());
+            }
+            catch (System.Exception ex)
+            {
+                return BadRequest("Upload Failed: " + ex.Message);
+            }
         }
 
         private bool AssetExists(Guid id)
