6a5c8eab0e75aa0fc349da766fed5b8c24623cef
hamed Mohammedi <hammedmohamadi@gmail.com>
2018-09-27 05:52:59 +0330

    admin

--- app/server/ClientApp/src/components/Admin/Admin.js
+++ app/server/ClientApp/src/components/Admin/Admin.js
@@ -1,22 +1,40 @@
 import React, { Component } from 'react';
 import { Glyphicon, Col, Grid, Row } from 'react-bootstrap';
 import './Admin.css';
+import { Statics } from '../Statics';
+import { Web3s, ViraCoinToken, ViraCoinCart } from '../Web3/Web3';
+import history from '../history';
 export class Admin extends Component {
     displayName = Admin.name
     constructor(props) {
         super(props);
-        this.state = { ToknOrCart: 0 };
+        Statics.IsLogin();
     }
     render() {
-        let Content = <h1>None</h1>;
-        if (this.state.ToknOrCart === 1) Content = <TokenAdmin />;
-        else if (this.state.ToknOrCart === 2) Content = <CartAdmin />;
-        else Content = <h1>None</h1>;
         return (
             <div className='PopUp'>
-                <button onClick={() => this.setState({ ToknOrCart: 1 })}>Token</button>
-                <button onClick={() => this.setState({ ToknOrCart: 2 })}>Cart</button>
-                {Content}
+                <div className='ComponentBaseStyle'>
+                    <Grid fluid>
+                        <Row>
+                            <Col md={6} className="AdminCol">
+                                <div className="AdminItem">
+                                    <h3>
+                                        Vira Token
+                                </h3>
+                                    <hr />
+                                </div>
+                            </Col>
+                            <Col md={6} className="AdminCol">
+                                <div className="AdminItem">
+                                    <h3>
+                                        Vira Cart
+                                </h3>
+                                    <hr />
+                                </div>
+                            </Col>
+                        </Row>
+                    </Grid>
+                </div>
             </div>
         );
     }
